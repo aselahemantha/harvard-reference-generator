7756cbb97a144305feade3401ffd919d7cb70f02
L.M.A.H. Premawansha <78247019+aselahemantha@users.noreply.github.com>
2021-10-12 10:00:14 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# .gitignore should include reference to config.py
-api_key = "1963830183:AAGRjqOb7UMe_cVjoXYpQZY8POkfrj8t_ps"
-api_secret = "YOUR_SECRET"
-access_token = "YOUR_ACCESS_TOKEN"
-token_secret = "YOUR_TOKEN_SECRET"
